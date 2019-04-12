a5ccb0afa0dfe70bafee2f57a0e567cd88202780
alemartinAG <ale.martin96@hotmail.com>
2019-04-12 19:50:28 -0300

    update and scanning working on internet

---
+++ Internet/client_cc.c
@@ -0,0 +1,311 @@
+/* Cliente en el dominio Unix - orientado a corrientes */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <errno.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+#include <string.h>
+
+#define SIZE 800 //buffSize de buffer
+#define ERROR 1 //codigo de error para exit
+#define CLEAR "\033[H\033[J" //borrar consola
+
+void scan();
+void telemetria();
+void update();
+void enviarDato(char[]);
+char * getUptime();
+
+int SAT_ID;
+char VERSION[] = {"3"};
+int socketFileDescr;
+
+
+int main(int argc, char *argv[]){
+
+	//Manejo manualmente un error en el pipe
+	signal(SIGPIPE, SIG_IGN);
+
+	/* genero un numero
+	aleatorio para el id
+	del satelite */
+	srand(time(NULL));
+	SAT_ID = rand();
+
+	int puerto;
+
+	//int socketFileDescr, sv_len;
+	//int sv_len;
+
+	struct sockaddr_in sv_addr;
+	struct hostent *server;
+
+	char buffer[SIZE];
+
+	int terminar = 0;
+
+    if(argc < 3){
+        printf("Uso: %s <host> <puerto>\n", argv[0]);
+        exit(0);
+    }
+
+    puerto = atoi(argv[2]);
+
+    printf("PUERTO: %d\n", puerto);
+
+    if((socketFileDescr = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		perror("creación de socket");
+		exit(ERROR);
+	}
+
+    server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		perror("Error, no existe el host");
+		exit(ERROR);
+	}
+
+    //printf(CLEAR);
+
+	memset((char *)&sv_addr, '\0', sizeof(sv_addr));
+	sv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&sv_addr.sin_addr.s_addr, server->h_length);
+	sv_addr.sin_port = htons(puerto);
+	//sv_len = strlen(sv_addr.sun_path) + sizeof(sv_addr.sun_family);
+
+	//Intento conectar al socket constantemente
+	while(1){
+		if(connect(socketFileDescr, (struct sockaddr *)&sv_addr, sizeof(sv_addr)) >= 0){
+			printf("\n-- Conectado --\n\n");
+			break;
+		}
+	}
+ 	
+
+	while(1){
+
+        memset(buffer, '\0', SIZE);
+
+        if(read(socketFileDescr, buffer, SIZE) < 0){
+            perror("lectura de socket");
+            exit(ERROR);
+        }
+
+  		switch(atoi(buffer)){
+  			
+  			case 0 :
+  			exit(0);
+
+  			case 1 :
+  			update();
+  			break;
+
+  			case 2 :
+  			telemetria();
+  			break;
+
+  			case 3 :
+  			scan();
+  			break;
+
+  			default :
+  			printf("RECIBO: %s\n", buffer);
+  		}
+	}
+
+	return 0;
+}
+
+void scan(){
+
+	int TAM = 1500;
+	FILE * fp;
+	int i = 0;
+	char buffer[TAM];
+	size_t bytes_read;
+
+	while(1){
+		
+		char filename[8] = {""};
+		sprintf(filename, "x%06d", i);
+		char filename2[20] = {"Image/"};
+		strcat(filename2, filename);
+		fp = fopen (filename2,"r");
+  		
+  		if (fp!=NULL)
+  		{
+  			memset(buffer, 0, TAM);
+		    bytes_read = fread(buffer, 1, TAM, fp);
+		    enviarDato(buffer);
+		    fclose (fp);
+
+			if(read(socketFileDescr, buffer, TAM) < 0){
+            	perror("lectura de socket");
+            	//exit(ERROR);
+            	return;
+        	}
+
+		    if(i%1000 == 0){
+		    	printf("Enviados: %d\n", i);
+		    }
+
+  		}
+  		else{
+  			//usleep(1500);
+  			printf("FIN DE ENVIO\n");
+  			enviarDato("FIN");
+  			break;
+  		}
+
+  		i++;
+  		//printf("Enviados: %d\n", i);
+  		//usleep(1000);
+	}
+
+}
+
+void update(){
+
+	printf("-UPDATE-\n");
+
+	int TAM = 15000;
+	char buffer[TAM];
+	char buffSize[sizeof(unsigned long)];
+	unsigned long tamBinario;
+
+	if(read(socketFileDescr, buffer, TAM) < 0){
+        perror("lectura de socket");
+        exit(ERROR);
+    }
+
+    if(read(socketFileDescr, buffSize, sizeof(unsigned long)) < 0){
+    	perror("lectura de socket");
+        exit(ERROR);
+    }
+
+    tamBinario = atoi(buffSize);
+
+    FILE *fp;
+
+    fp = fopen("client_u", "wb");
+    fwrite(buffer, 1, tamBinario, fp);
+    fclose(fp);
+
+	enviarDato("FIN");
+
+	printf("REBOOTING...\n");
+	system("./restart.sh");
+
+}
+
+void telemetria(){
+
+	/*---Conexión UDP---*/
+
+	int descriptor_udp, resultado, cantidad, i, puerto;
+	struct sockaddr_in struct_cliente;
+	socklen_t tamano_direccion;
+	char buffer[SIZE];
+	struct hostent *server;
+
+	server = gethostbyname("localhost");
+	if ( server == NULL ) {
+		printf("ERROR, no existe el host\n");
+		return;
+	}
+
+	usleep(1000);
+
+	puerto = 27815;
+
+	/* Creacion de socket */
+	if((descriptor_udp = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+		perror("socket" );
+		return;
+	}
+
+	/* Inicialización y establecimiento de la estructura del cliente */
+	//memset(&struct_cliente, 0, sizeof(struct_cliente));
+	struct_cliente.sin_family = AF_INET;
+	struct_cliente.sin_port = htons(puerto);
+	struct_cliente.sin_addr = *( (struct in_addr *)server->h_addr );
+	memset( &(struct_cliente.sin_zero), '\0', 8 );
+	//strncpy(struct_cliente.sun_path, nom_sock, sizeof(struct_cliente.sun_path));
+
+
+	/*-- Leo uptime del sistema --*/
+	FILE *fp = NULL;
+    char buff_uptime[SIZE] = "";
+    size_t bytes_read;
+    char data_found[64] = "";
+
+    //Abro el archivo, lo leo y lo guardo en el buffer
+    fp = fopen("/proc/uptime", "r");
+    bytes_read = fread(buff_uptime, 1, sizeof(buff_uptime), fp);
+    fclose(fp);
+
+    if(bytes_read == 0 || bytes_read == sizeof(buff_uptime)){
+        printf("buffer problem");
+    }
+
+    //le agrego un caracter de finalizacion al ultimo
+    buff_uptime[bytes_read] = '\0';
+
+    int uptime;
+    sscanf(buff_uptime, "%d", &uptime);
+    sprintf(buff_uptime, "Uptime: %02dD %02d:%02d:%02d \n", (uptime / 60 / 60 / 24), (uptime / 60 / 60 % 24), (uptime / 60 % 60),
+           (uptime % 60));
+
+    
+    /*---Message Parsing---*/
+
+	char id[sizeof(SAT_ID)];
+	memset(id, '\0', sizeof(id));
+	sprintf(id, "%d", SAT_ID);
+
+	char catid[SIZE] = {"ID: "};
+	strcat(catid, id);
+	//enviarDato(catid); //envio el id del satelite
+
+	char catver[SIZE] = "SOFTWARE VERSION: ";
+	strcat(catver, VERSION);
+
+	memset(buffer, 0, sizeof(buffer)); //Limpio el buffer
+	strcpy(buffer, catid);
+	strcat(buffer, ";");
+	strcat(buffer, catver);
+	strcat(buffer, ";");
+	strcat(buffer, buff_uptime);
+
+
+	cantidad = strlen(buffer);
+
+	/* Envío de datagrama al servidor */
+	resultado = sendto(descriptor_udp, buffer, cantidad, 0, (struct sockaddr *)&struct_cliente, sizeof(struct_cliente));
+	if(resultado < 0) {
+ 		perror("ERROR EN ENVIO UDP");
+		//exit(ERROR);
+	}
+
+	/*if(close(descriptor_udp) < 0){
+        perror("ERROR CERRANDO SOCKET UDP");
+    }*/
+
+    //enviarDato("FIN");
+	
+}
+
+void enviarDato(char dato[]){
+
+	if(write(socketFileDescr, dato, strlen(dato)) < 0){
+        perror("escritura de socket");
+        exit(ERROR);
+    }
+
+    //usleep(1300);
+}
---
+++ Internet/server_cc.c
@@ -0,0 +1,520 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h> 
+#include <stdbool.h>
+#include <string.h>
+#include <zconf.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define QUEUE 1	//tamaño maximo de la cola de conexiones pendientes
+#define SIZE 256 //tamaño del buffer
+#define PASS "123" //contraseña
+#define USER "admin" //nombre de usuario
+#define TRIES 3 //cantidad de intentos de identicacion
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define CRESET   "\x1b[0m"
+#define CLEAR "\033[H\033[J" //borrar consola
+#define ERROR 1 //codigo de error
+
+#define UPDATE 1
+#define TELE 2
+#define SCAN 3
+#define EXIT 4
+
+#define TAMIMG 1500
+
+bool registro(char[], char[]);
+void identificar();
+int checkCommand(char[]);
+void updateFirmware();
+char * leerArchivo(char[], bool);
+void conectarSocket(char[]);
+void conectarUDP();
+void getImagenSatelital();
+
+unsigned long tamBinario;
+int socketFileDescr, newSockFd;
+
+const char *commands[4] = { "update firmware.bin", 
+                            "obtener telemetria", 
+                            "start scanning",
+                            "exit"
+                        };
+
+int main(int argc, char *argv[]){
+
+    //Manejo manualmente un error en el pipe
+    signal(SIGPIPE, SIG_IGN);
+    
+    char buffer[SIZE];
+
+    if(argc != 2) {
+        printf("Uso: %s <puerto>\n", argv[0]);
+        exit(ERROR);
+    }
+
+    identificar();
+
+    char argumento[SIZE];
+    strcpy(argumento, argv[1]);
+
+    conectarSocket(argumento);
+
+    //printf(CLEAR);
+
+    while(1){
+
+        bool receiving = true;
+        memset(buffer, 0, sizeof(buffer)); //Limpio el buffer
+
+        printf(MAGENTA"%s"CYAN"@EstacionTerrena:"CRESET"~$ ", USER);
+        fgets(buffer, SIZE-1, stdin);
+
+        buffer[strlen(buffer)-1] = '\0';
+
+        int num = checkCommand(buffer);
+
+        memset(buffer, 0, sizeof(buffer)); //Limpio el buffer
+        sprintf(buffer, "%d", num);
+
+        //Si el comando existe lo envio
+        if(num >= 0){
+
+            if(num == EXIT){
+                if(close(socketFileDescr) < 0){
+                    perror("ERROR CERRANDO SOCKET");
+                }
+
+                exit(0);
+            }
+
+            if(write(newSockFd, buffer, strlen(buffer)) < 0){
+                perror("\n\nEL CLIENTE SE HA DESCONECTADO");
+                conectarSocket(argumento);
+
+                if(write(newSockFd, buffer, strlen(buffer)) < 0){
+                    perror("ERROR AL VOLVER A CONECTAR");
+                    exit(ERROR);
+                }
+                //exit(ERROR);
+            }
+
+            //Update Firmware.bin
+            if(num == UPDATE){
+                updateFirmware();
+                system("./version.sh"); //corro shell script
+                //TODO: borrar update despues de pasarlo
+                char *bufferUpdate = leerArchivo("update", true);
+
+                printf("strlen bufferUpdate: %lu\n", strlen(bufferUpdate));
+                printf("sizeof bufferUpdate: %lu\n", sizeof(bufferUpdate));
+                printf("sizeof * bufferUpdate: %lu\n", sizeof( * bufferUpdate));
+
+                if(write(newSockFd, bufferUpdate, tamBinario) < 0){
+                    perror("escritura de socket");
+                    exit(ERROR);
+                }
+
+                free(bufferUpdate);
+
+                usleep(1000);
+
+                char buffSize[sizeof(unsigned long)];
+                sprintf(buffSize, "%lu", tamBinario);
+
+                if(write(newSockFd, buffSize, sizeof(unsigned long)) < 0){
+                    perror("escritura de socket");
+                    exit(ERROR);
+                }
+
+                usleep(1000);
+                close(socketFileDescr);
+                
+                system("./removeUpdate.sh");
+
+                conectarSocket(argumento);
+                //receiving = false;
+
+            }
+
+            if(num == TELE){
+                conectarUDP();
+            }
+
+            if(num == SCAN){
+                getImagenSatelital();
+            }
+
+            //printf("\n");
+
+            /*while(receiving){
+
+                memset(buffer, 0, sizeof(buffer));
+
+                if(read(newSockFd, buffer, SIZE) < 0){
+                    perror("lectura de socket");
+                    exit(ERROR);
+                }
+
+                if(!strcmp(buffer, "FIN")){
+                    receiving = false;
+                }
+                else{
+                    printf("- %s\n", buffer);
+                }
+            }*/
+
+        }
+
+    }
+}
+
+void getImagenSatelital(){
+
+    bool receiving = true;
+    char buffer[TAMIMG] = {""};
+
+    FILE *fp = NULL;
+    int i=0;
+
+    printf("Obteniendo Imagen Satelital\n");
+
+    while(receiving){
+
+        char filename[8] = {""};
+        sprintf(filename, "x%06d", i);
+        char filename2[20] = {"Recibido/"};
+        strcat(filename2, filename);
+
+        memset(buffer, 0, sizeof(buffer));
+        //char buffer[TAMIMG] = {""};
+
+        if(read(newSockFd, buffer, TAMIMG) < 0){
+            perror("lectura de socket");
+            exit(ERROR);
+        }
+
+        if(!strcmp(buffer, "FIN")){
+            receiving = false;
+            printf("Imagen Obtenida\n");
+        }
+        else{
+            
+            fp = fopen(filename2, "w");
+            
+            if(fp != NULL){
+                fwrite(buffer, 1, strlen(buffer), fp);
+                fclose(fp);
+                write(newSockFd, "ok", 2);
+            }
+            else{
+                perror("ERROR AL ABRIR ARCHIVO");
+            }
+            
+            
+            //strcat(bufferSat, buffer);
+
+            i++;
+        }
+    }
+
+    printf("Procesando Imagen...\n");
+
+    system("./generarImagen.sh");
+    
+
+    /*fp = fopen("scan_satelital", "w");
+    fwrite(bufferSat, 1, TAMIMG, fp);
+    fclose(fp);*/
+
+}
+
+void updateFirmware(){
+
+    int versionActual;
+
+    printf("-UPDATE-\n");
+
+    //char buffer[8000];
+
+    //char archivo[SIZE] = {"client_cc.c"};
+    //strcpy(buffer, leerArchivo("client_cc.c"));
+    char * buffer = leerArchivo("client_cc.c", false);
+
+    //printf("BUFFER:\n\n%s\n", buffer);
+
+    char * match;
+    char data_found[64] = "";
+
+    char seccion[SIZE] = {"char VERSION[] ="};
+    match = strstr(buffer, seccion);
+    sscanf(match, "%[^\n]", data_found);
+    
+    char *token;
+
+    /* get the first token */
+    token = strtok(data_found, "\"");
+
+    char replace[SIZE];
+    strcpy(replace, token);
+    strcat(replace, "\"");
+
+    /* walk through other tokens */
+    while(token != NULL) {
+
+        if(strlen(token) <= 2){
+            versionActual = atoi(token);
+            break;
+        }
+
+        token = strtok(NULL, "\"");
+    }
+
+    versionActual++;
+
+    char version[SIZE];
+    sprintf(version, "%d", versionActual);
+    strcat(replace, version);
+    strcat(replace, "\"};");
+
+    strncpy(match, replace, strlen(replace));
+
+    FILE *fp;
+
+    fp = fopen("client_cc.c", "w");
+    fwrite(buffer, 1, strlen(buffer), fp);
+    fclose(fp);
+
+    free(buffer);
+
+}
+
+char * leerArchivo(char file[], bool binary){
+
+    FILE *fp;
+    unsigned long TAM;
+
+    //abro el archivo el modo lectura
+    if(binary){
+        fp = fopen(file, "rb"); // modo de lectura binario
+    }
+    else{ 
+        fp = fopen(file, "r"); // modo de lectura
+    }
+    if (fp == NULL){
+        perror("ERROR abriendo archivo\n");
+        exit(ERROR);
+    }
+
+    if(binary){
+        TAM = sizeof(int) * 15000;
+    }
+    else{
+        TAM = sizeof(char) * 8000;
+    }
+
+    char * buffer = malloc(TAM);
+    size_t bytes_read;
+
+    if(binary){
+        bytes_read = fread(buffer, sizeof(int), TAM, fp);
+        tamBinario = bytes_read * sizeof(int);
+    }
+    else{
+        bytes_read = fread(buffer, 1, TAM, fp);
+    }
+    
+    if(!binary){
+        buffer[bytes_read] = '\0';
+    }
+    
+    fclose(fp);
+
+    return buffer;
+}
+
+int checkCommand(char reading[]){
+
+    size_t cant = sizeof(commands)/sizeof(commands[0]);
+
+    for(int i=1; i<=cant; i++){
+        if(!strcmp(reading, commands[i-1])){
+            return i;
+        }
+    }
+
+    printf("Comando '%s' no encontrado\n", reading);
+
+    return -1;
+
+}
+
+//Ingreso y lectura para usuario y contraseña
+bool registro(char print[], char comp[]){
+
+    char input[SIZE];
+
+    //leemos la consola
+    printf("\n%s: ", print);
+    fgets(input, SIZE, stdin);
+    input[strlen(input)-1] = '\0';
+
+    if(!strcmp(input, comp)){
+        return true;
+    }
+
+    return false;
+
+}
+
+//Identificarse en el servidor
+void identificar(){
+
+    //Para proceso de identificacion
+    int intentos = 0;
+    bool identified = false;
+
+    while(!identified){
+
+        if(registro("USER", USER) && registro("PASS", PASS)){
+            identified = true;
+            printf("\n***Autentificado correctamente***\n\n");
+        }
+        else{
+
+            intentos++;
+
+            if(intentos == TRIES){
+                perror("\nERROR en la identificacion (Demasiados Intentos)\n");
+                exit(ERROR);
+            }
+
+            printf("\nNombre de usuario y/o contraseña incorrecto, quedan %d intentos\n", 3-intentos);
+        }
+
+    }
+}
+
+void conectarSocket(char argv[]){
+
+    int pid, sv_len, puerto, cl_len;
+    struct sockaddr_in sv_addr, cl_addr;
+
+    printf("\n-- Esperando la conexion con el satelite --\n");
+
+    socketFileDescr = socket(AF_INET, SOCK_STREAM, 0);
+    int on = 1;
+    setsockopt(socketFileDescr, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+    if(socketFileDescr < 0){
+        perror("\nERROR abriendo el socket");
+        exit(ERROR);
+    }
+
+    //unlink(argv); //Remuevo el nombre de archivo si existe
+
+    bzero((char *) &sv_addr, sizeof(sv_addr)); //Limpio la direccion del sv
+
+    puerto = atoi(argv);
+
+    sv_addr.sin_family = AF_INET;
+    sv_addr.sin_addr.s_addr = INADDR_ANY;
+    sv_addr.sin_port = htons(puerto);
+    
+    //strcpy(sv_addr.sun_path, argv);
+    //sv_len = strlen(sv_addr.sun_path) + sizeof(sv_addr.sun_family);
+
+    if(bind(socketFileDescr, (struct sockaddr*)&sv_addr, sizeof(sv_addr)) < 0){
+        perror("\nERROR en el binding");
+        exit(ERROR);
+    }
+
+    printf( "Proceso: %d - socket disponible: %d\n", getpid(), ntohs(sv_addr.sin_port));
+
+    //acepto conexiones
+    listen(socketFileDescr, QUEUE);
+
+    cl_len = sizeof(cl_addr);
+
+    newSockFd = accept(socketFileDescr, (struct sockaddr*)&cl_addr, &cl_len);
+
+    if(newSockFd < 0){
+        perror("\nERROR en accept");
+        exit(ERROR);
+    }
+
+    printf("\n--- Conexion Exitosa ---\n");
+
+    sleep(1);
+    printf(CLEAR);
+
+}
+
+void conectarUDP(){
+
+    int socket_udp, resultado;
+    struct sockaddr_in struct_servidor;
+    socklen_t tamano_direccion;
+    char buffer[SIZE] = {""};
+
+    int puerto = 27815;
+
+    memset(buffer, '\0', SIZE);
+
+    char nom_sock[SIZE] = {"UDPsocket"};
+
+    /* Creacion de socket */
+    if((socket_udp = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+        perror("socket");
+        //exit(ERROR);
+    }
+
+    /* Remover el nombre de archivo si existe */
+    //unlink(nom_sock);
+
+    /* Inicialización y establecimiento de la estructura del servidor */
+    memset(&struct_servidor, 0, sizeof(struct_servidor));
+    struct_servidor.sin_family = AF_INET;
+    struct_servidor.sin_addr.s_addr = INADDR_ANY;
+    struct_servidor.sin_port = htons(puerto);
+    memset( &(struct_servidor.sin_zero), '\0', 8 );
+    //strncpy(struct_servidor.sun_path, nom_sock, sizeof(struct_servidor.sun_path));
+
+    /* Ligadura del socket de servidor a una dirección */
+    if((bind(socket_udp, (struct sockaddr *)&struct_servidor, sizeof(struct_servidor))) < 0 ) {
+        perror("bind");
+        //exit(ERROR);
+    }
+
+    tamano_direccion = sizeof(struct sockaddr);
+    /* Mantenimiento de un lazo infinito, aceptando conexiones */
+    resultado = recvfrom (socket_udp, buffer, SIZE, 0, (struct sockaddr *) &struct_servidor, &tamano_direccion);
+    if(resultado < 0) {
+        perror("recepción");
+        //exit(ERROR);
+    }
+    else{
+
+        char *token;
+        token = strtok(buffer, ";");
+
+        /* walk through other tokens */
+        while(token != NULL) {
+            printf("- %s\n", token);
+            token = strtok(NULL, ";");
+        }
+
+    }
+
+
+
+    if(close(socket_udp) < 0){
+        //perror("ERROR CERRANDO SOCKET UDP");
+    }
+
+}
